efe725c289b0fd179d8aa30952b1e13e71d2c518
travis-ci <travis@travis-ci.org>
2020-09-08 10:19:36 +0000

    Auto-push pattern to gh-pages successful. (Travis build: 43)

---
+++ snapshot/service-worker.js
@@ -0,0 +1,177 @@
+var cacheName = 'cache_1599560359268';
+
+var filesToCache = [  
+'/gmd-table-demo/',
+
+'GmdDataTableDemo/1F74861B01E9B669BBBDE69812F7E867.cache.js',
+
+'GmdDataTableDemo/3CA359423A370BF379DD1BCCB4D036C8.cache.js',
+
+'GmdDataTableDemo/B47EC69222B403EC95103617ADA1C984.cache.js',
+
+'GmdDataTableDemo/GmdDataTableDemo.devmode.js',
+
+'GmdDataTableDemo/GmdDataTableDemo.nocache.js',
+
+'GmdDataTableDemo/clear.cache.gif',
+
+'GmdDataTableDemo/css/animation.css',
+
+'GmdDataTableDemo/css/animation.min.css',
+
+'GmdDataTableDemo/css/fontawesome-all.css',
+
+'GmdDataTableDemo/css/fontawesome-all.min.css',
+
+'GmdDataTableDemo/css/material-icons.css',
+
+'GmdDataTableDemo/css/material-icons.min.css',
+
+'GmdDataTableDemo/css/materialize.blue.css',
+
+'GmdDataTableDemo/css/materialize.blue.min.css',
+
+'GmdDataTableDemo/css/materialize.css',
+
+'GmdDataTableDemo/css/materialize.min.css',
+
+'GmdDataTableDemo/font/material-icons/MaterialIcons-Regular.eot',
+
+'GmdDataTableDemo/font/material-icons/MaterialIcons-Regular.ttf',
+
+'GmdDataTableDemo/font/material-icons/MaterialIcons-Regular.woff',
+
+'GmdDataTableDemo/font/material-icons/MaterialIcons-Regular.woff2',
+
+'GmdDataTableDemo/font/roboto/Roboto-Bold.ttf',
+
+'GmdDataTableDemo/font/roboto/Roboto-Bold.woff',
+
+'GmdDataTableDemo/font/roboto/Roboto-Bold.woff2',
+
+'GmdDataTableDemo/font/roboto/Roboto-Light.ttf',
+
+'GmdDataTableDemo/font/roboto/Roboto-Light.woff',
+
+'GmdDataTableDemo/font/roboto/Roboto-Light.woff2',
+
+'GmdDataTableDemo/font/roboto/Roboto-Medium.ttf',
+
+'GmdDataTableDemo/font/roboto/Roboto-Medium.woff',
+
+'GmdDataTableDemo/font/roboto/Roboto-Medium.woff2',
+
+'GmdDataTableDemo/font/roboto/Roboto-Regular.ttf',
+
+'GmdDataTableDemo/font/roboto/Roboto-Regular.woff',
+
+'GmdDataTableDemo/font/roboto/Roboto-Regular.woff2',
+
+'GmdDataTableDemo/font/roboto/Roboto-Thin.ttf',
+
+'GmdDataTableDemo/font/roboto/Roboto-Thin.woff',
+
+'GmdDataTableDemo/font/roboto/Roboto-Thin.woff2',
+
+'GmdDataTableDemo/fontawesome-icons/fa-brands-400.eot',
+
+'GmdDataTableDemo/fontawesome-icons/fa-brands-400.ttf',
+
+'GmdDataTableDemo/fontawesome-icons/fa-brands-400.woff',
+
+'GmdDataTableDemo/fontawesome-icons/fa-brands-400.woff2',
+
+'GmdDataTableDemo/fontawesome-icons/fa-regular-400.eot',
+
+'GmdDataTableDemo/fontawesome-icons/fa-regular-400.ttf',
+
+'GmdDataTableDemo/fontawesome-icons/fa-regular-400.woff',
+
+'GmdDataTableDemo/fontawesome-icons/fa-regular-400.woff2',
+
+'GmdDataTableDemo/fontawesome-icons/fa-solid-900.eot',
+
+'GmdDataTableDemo/fontawesome-icons/fa-solid-900.ttf',
+
+'GmdDataTableDemo/fontawesome-icons/fa-solid-900.woff',
+
+'GmdDataTableDemo/fontawesome-icons/fa-solid-900.woff2',
+
+'index.html',
+
+'launcher-icons/launcher1x.png',
+
+'launcher-icons/launcher2x.png',
+
+'launcher-icons/launcher4x.png',
+
+'launcher-icons/launcher5x.png',
+
+'splash/font/Roboto-Regular.eot',
+
+'splash/font/Roboto-Regular.ttf',
+
+'splash/font/Roboto-Regular.woff',
+
+'splash/font/Roboto-Regular.woff2',
+
+'splash/splash.css',
+
+'splash/splash.js'
+    ];
+
+
+/**
+ * The install event is your chance to cache everything you need before being able to control clients. The promise you
+ * pass to event.waitUntil() lets the browser know when your install completes, and if it was successful.
+ */
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll(filesToCache)
+                .then(() => self.skipWaiting());
+        })
+    );
+});
+
+/**
+ * Once your service worker is ready to control clients and handle functional events like push and sync, you'll get an
+ * activate event. But that doesn't mean the page that called .register() will be controlled.
+ */
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(oldCache) {
+                    if (oldCache !== cacheName) {
+                        console.log('ServiceWorker : Deleting old cache:', oldCache);
+                        return caches.delete(oldCache);
+                    }
+                })
+            );
+        }).then(function() {
+            console.log('ServiceWorker : Claiming clients for version', cacheName);
+            return self.clients.claim();
+        })
+    );
+});
+
+/**
+ * It contains information about the fetch, including the request and how the receiver will treat the response.
+ * It provides the event.respondWith() method, which allows us to provide a response to this fetch.
+ */
+self.addEventListener('fetch', event => {
+    if (event.request.method !== 'GET') {
+        /* If we don't block the event as shown below, then the request will go to
+           the network as usual.
+        */
+        return;
+    }
+    event.respondWith(
+        caches.open(cacheName)
+            .then(cache => cache.match(event.request, {ignoreSearch: true}))
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
